9ee2e8fd5494e84d050390ce1d9b8f94f505dc93
Ankith3113 <ankith3113@gmail.com>
2023-03-08 14:29:17 +0530

    Vendor and customer dashboard

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,6 +15,9 @@ urlpatterns = [
     path('forgot_password/', views.forgot_password, name='forgot_password'),
     path('reset_password_validate/<uidb64>/<token>/', views.reset_password_validate, name='reset_password_validate'),
     path('reset_password/', views.reset_password, name='reset_password'),
-
+    path('accounts/', views.myAccount),
+    path('vendor/', include('vendor.urls')),
+    path('', views.myAccount),
+    
 
 ] 
